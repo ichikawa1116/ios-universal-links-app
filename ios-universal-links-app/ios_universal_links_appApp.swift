4f23d3ea074b5d83cb35539625e4d9f197e37454
ichikawa1116 <ku.komaruha@icloud.com>
2025-04-02 12:24:45 +0900

    Initial Commit

---
+++ ios-universal-links-app/ios_universal_links_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_universal_links_appApp.swift
+//  ios-universal-links-app
+//
+//  Created by Manami Ichikawa on 2025/04/02.
+//
+
+import SwiftUI
+
+@main
+struct ios_universal_links_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-universal-links-appTests/ios_universal_links_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_universal_links_appTests.swift
+//  ios-universal-links-appTests
+//
+//  Created by Manami Ichikawa on 2025/04/02.
+//
+
+import Testing
+@testable import ios_universal_links_app
+
+struct ios_universal_links_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
